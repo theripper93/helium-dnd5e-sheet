ad7ace7978a9f4249053adf049d8f96f84f59a66
theripper93 <tsplab@gmail.com>
2023-06-09 16:16:04 +0200

    started refactor to eventually get rid of the abstract class

--- scripts/sheets/character.js
+++ scripts/sheets/character.js
@@ -1,15 +1,48 @@
-import { HeliumActorSheet } from "./abstract/HeliumActorSheet.js";
 import { MODULE_ID } from "../consts.js";
 
-export class HeliumCharacterSheet extends HeliumActorSheet {
+export class HeliumCharacterSheet extends game.system.applications.actor.ActorSheet5eCharacter {
     static get type() {
         return "character";
     }
 
-    async getData(options) {
-        const context = await super.getData(options);
-        console.log(context);
-        return mergeObject(context, { });
+    static get documentClass() {
+        return Actor;
+    }
+
+    static get template() {
+        return `modules/${MODULE_ID}/templates/sheets/actor/${this.type}.hbs`;
+    }
+
+    get template() {
+        return `modules/${MODULE_ID}/templates/sheets/actor/${this.constructor.type}.hbs`;
+    }
+
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            height: 720,
+            width: 800,
+            template: this.template,
+            id: `${MODULE_ID}-${this.type}-sheet`,
+            classes: [`${MODULE_ID}`, `${MODULE_ID}-sheet`, `${MODULE_ID}-${this.type}-sheet`],
+        });
+    }
+
+    async getData() {
+        const context = await super.getData();
+        const items = Array.from(this.actor.items);
+        const actorActionsTypes = ["action", "bonus", "reaction", "special", "legendary", "lair", "mythic"];
+        const actorItemTypes = ["weapon", "equipment", "feature", "spell"]
+        const actorActions = {}
+        actorActionsTypes.forEach((type) => {
+            const actionItems = items.filter((item) => item?.system?.activation?.type === type && actorItemTypes.includes(item.type));
+            if(!actionItems.length) return;
+            actorActions[type] = {
+                items: actionItems,
+                label: CONFIG.DND5E.abilityActivationTypes[type]
+            }
+        });
+        context.actorActions = actorActions;
+        return context;
     }
 
     activateListeners(html) {
